49ccd7d3fcd4e60deea2e4d681bbfd8f52df42f8
Nadali Khalili <nadalikh@gmail.com>
2021-12-14 09:50:53 +0330

    and, or, xor and their nots

--- package/testMain.go
+++ package/testMain.go
@@ -9,6 +9,6 @@ func main() {
 	//bitwise.And(uint8(strconv.ParseInt("00000001", 2, 64)),uint8(strconv.ParseInt("00000001", 2, 8)))
 	var a byte = 5
 	var b byte = 3
-	fmt.Println(bitwise.Or(a, b))
+	fmt.Println(bitwise.Xor(a, b))
 	fmt.Println(bitwise.Nor(a, b))
 }
